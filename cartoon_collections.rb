f4fd8cae74b69c24e5605371a4d2e3651196f4d8
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-13 15:10:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,9 @@ def find_the_cheese(ary_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
   # default memo to false (not found)
   cheese = cheese_types.inject(nil) { | memo_a, obj_a |
-    memo_a = obj_a if ary_items.include?(obj_a) else nil
+    memo_a = ary_items.inject(nil) { | memo_b, obj_b |
+      memo_b = obj_b if obj_a == obj_b
+    }
   }
   return cheese
 end
