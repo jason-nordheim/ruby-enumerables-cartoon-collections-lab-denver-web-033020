9d38a8247a4fcd9fba66f83d730a558053d0f0c1
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-13 15:06:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
+  result = array.map.with_index { | n,i | p"#{i+1}. #{n}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  result = array.map { |n| "#{n.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(ary_items)
+  # code an argument here
   # Your code here
+  ary_items.any? { |e| e.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ary_items)
+  # input: A list of items, one of which should be a cheese in the cheese list
+  # output: the type of cheese matched with one on the list below
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # default memo to false (not found)
+  cheese = cheese_types.inject(nil) {| memo_a, obj_a |
+    memo_a = obja if ary_items.include(obj_a) else nil
+  }
+  end
+  return cheese 
 end
